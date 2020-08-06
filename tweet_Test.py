78596ad32f33d7b1e40129a0c193ab552d63f8a5
YashikaSharma123 <48388366+YashikaSharma123@users.noreply.github.com>
2020-08-06 18:11:36 +0530

    Add files via upload
    add some files (for bar graph and some functions)

--- tweet_Test.py
+++ tweet_Test.py
@@ -3,54 +3,52 @@ import tweepy #access to tweet app
 from tweepy import OAuthHandler #authenication 
 from textblob import TextBlob #text/tweet parse
  
-# keys and tokens from the Twitter Dev Console
-#2 key used because one for sign one for data
-consumer_key = ''
-consumer_secret = ''
-access_token = ''
-access_token_secret = ''
+class TwitterClient(object):
+    def __init__(self):
+        # keys and tokens from the Twitter Dev Console
+        consumer_key = 'TL7RyLnfilYH6xaoAlS0XFDCg'
+        consumer_secret = 'u5uBn4P62PMIxT4uwUVTl1Ycx4rsfByFs6jl2e4SQ9zDjPIzCO'
+        access_token = '919434545924935681-2woCDEXuXQdhJewDaCRBqHBYmi5SFDN'
+        access_token_secret = 'T29jqUm6rZqsRYO7AGc47GlgYTaAaN5OtJD0DATo1uBjh'
  
-# attempt authentication
-try:
+        # attempt authentication
+        try:
             # create OAuthHandler object
-            auth = OAuthHandler(consumer_key, consumer_secret)
+            self.auth = OAuthHandler(consumer_key, consumer_secret)
             # set access token and secret
-            auth.set_access_token(access_token, access_token_secret)
+            self.auth.set_access_token(access_token, access_token_secret)
             # create tweepy API object to fetch tweets
-            api = tweepy.API(auth)
+            self.api = tweepy.API(self.auth)
             print('auth. success')
             
             
-except:
+        except:
             print("Error: Authentication Failed")
  
-def clean_tweet( tweet):
-
-        s =' '        
-        return s.join(re.sub("(@[A-Za-z0-9]+)|([^0-9A-Za-z \t]) |(\w+:\/\/\S+)", " ", tweet).split())
-        
+    def clean_tweet(self, tweet):
+        '''
+        Utility function to clean tweet text by removing links, special characters
+        using simple regex statements.
+        '''
+        return ' '.join(re.sub("(@[A-Za-z0-9]+)|([^0-9A-Za-z \t]) |(\w+:\/\/\S+)", " ", tweet).split())
+    
  
-def get_tweet_sentiment( tweet):
+    def get_tweet_sentiment(self, tweet):
         '''
         Utility function to classify sentiment of passed tweet
         using textblob's sentiment method
         '''
         # create TextBlob object of passed tweet text
-        analysis = TextBlob(clean_tweet(tweet))
-        #print('analysis',analysis)
+        analysis = TextBlob(self.clean_tweet(tweet))
         # set sentiment
         if analysis.sentiment.polarity > 0:
-            #print('0',analysis.sentiment.polarity )
             return 'positive'
         elif analysis.sentiment.polarity == 0:
-            #print('0',analysis.sentiment.polarity )
-
             return 'neutral'
         else:
-            #print('0',analysis.sentiment.polarity )
             return 'negative'
  
-def get_tweets( query, count=10):
+    def get_tweets(self, query, count=10):
         '''
         Main function to fetch tweets and parse them.
         '''
@@ -59,20 +57,17 @@ def get_tweets( query, count=10):
  
         try:
             # call twitter api to fetch tweets
-            fetched_tweets = api.search(q = query, count = count)
-            #print("fetch",fetched_tweets)
+            fetched_tweets = self.api.search(q = query, count = count)
  
             # parsing tweets one by one
             for tweet in fetched_tweets:
                 # empty dictionary to store required params of a tweet
                 parsed_tweet = {}
  
-            
                 # saving text of tweet
                 parsed_tweet['text'] = tweet.text
                 # saving sentiment of tweet
-                parsed_tweet['sentiment'] = get_tweet_sentiment(tweet.text)
-                #print('text',parsed_tweet)
+                parsed_tweet['sentiment'] = self.get_tweet_sentiment(tweet.text)
  
                 # appending parsed tweet to tweets list
                 if tweet.retweet_count > 0:
@@ -92,25 +87,17 @@ def get_tweets( query, count=10):
 
 def get_tweet_result(leader):    
 
-   
+    #print(leader)
+     # creating object of TwitterClient Class
+    api = TwitterClient()
     # calling function to get tweets
-    tweets = get_tweets(query = leader, count = 200)
+    tweets = api.get_tweets(query = leader, count = 200)
     #print(tweets)
-
-    #conventional list
-    #a =[111,222,4444]
-    #b = [x*2 for x in a]
-    #b=[]
-    #for x in a:
-    #    b.append(x*2)
-        
-        
     
     # picking positive tweets from tweets
     ptweets = [tweet for tweet in tweets if tweet['sentiment'] == 'positive']
     
-    result=[];
-    
+    result=[];                           
     # percentage of positive tweets
     #print("Positive tweets percentage: {} %".format(100*len(ptweets)/len(tweets)))
     result.append(format(100*len(ptweets)/len(tweets)))
@@ -135,9 +122,6 @@ def gettweet():
         res.append(get_tweet_result(l))
         
     print(res)
-    
+    return res
     
 gettweet()
-
-
-
